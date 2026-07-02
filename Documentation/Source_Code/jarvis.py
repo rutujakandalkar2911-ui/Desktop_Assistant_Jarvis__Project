fbf8e13335c862b1c933a7a8eedcaf3d1ccf21bb
rutujakandalkar2911-ui <rutujakandalkar2911@gmail.com>
2026-07-02 17:53:28 +0530

    Update jarvis.py

--- Documentation/Source_Code/jarvis.py
+++ Documentation/Source_Code/jarvis.py
@@ -1,1 +1,52 @@
-# Desktop Assistant Jarvis Source Code
+import pyttsx3
+import datetime
+import speech_recognition as sr
+import webbrowser
+
+engine = pyttsx3.init()
+
+def speak(text):
+    engine.say(text)
+    engine.runAndWait()
+
+def wish():
+    hour = datetime.datetime.now().hour
+    if hour < 12:
+        speak("Good Morning")
+    elif hour < 18:
+        speak("Good Afternoon")
+    else:
+        speak("Good Evening")
+    speak("I am Jarvis. How can I help you?")
+
+def take_command():
+    r = sr.Recognizer()
+    with sr.Microphone() as source:
+        print("Listening...")
+        audio = r.listen(source)
+
+    try:
+        command = r.recognize_google(audio)
+        print(command)
+        return command.lower()
+    except:
+        return ""
+
+wish()
+
+while True:
+    command = take_command()
+
+    if "youtube" in command:
+        webbrowser.open("https://www.youtube.com")
+
+    elif "google" in command:
+        webbrowser.open("https://www.google.com")
+
+    elif "time" in command:
+        current_time = datetime.datetime.now().strftime("%H:%M:%S")
+        speak(current_time)
+
+    elif "exit" in command:
+        speak("Goodbye")
+        break
